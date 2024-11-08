d8b3f0c1306bc33047f654ddff45a54e5463623f
titosukamenabung <titosukamenabung@gmail.com>
2024-11-08 09:11:02 +0700

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,11 @@
+nama = "tito"
+tahun_sekarang = int(input("masukan tahun sekarang: "))
+tahun_lahir = int(input("masukan tahun lahir: "))
+umur = tahun_sekarang - tahun_lahir
+
+print(f"{nama}")
+print(tahun_sekarang)
+print(tahun_lahir)
+print("umur anda adalah :", umur , "tahun ")
+
+# memangil fungsi untuk menjalankan progam
